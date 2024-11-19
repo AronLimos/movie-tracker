dffd4899d97b40db5ebf837ca51c72f200150057
AronLimos <aron.jlimos@gmail.com>
2024-11-19 14:15:49 -0700

    Added Buttons and Style MainWindow

---
+++ TrackFlix/EditMovieWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TrackFlix1;
+
+namespace TrackFlix
+{
+    /// <summary>
+    /// Interaction logic for EditMovieWindow.xaml
+    /// </summary>
+    public partial class EditMovieWindow : Window
+    {
+        public Movie EditMovie { get; set; } // Reference to MainWIndow
+        public EditMovieWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEditMovieClick(object sender, RoutedEventArgs e)
+        {
+            // Code here and logic here
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            /********************************************
+           * Indicates that the user canceled the operation,
+           * preventing any movie from being updated, and closes the window.
+           ********************************************/
+
+            DialogResult = false;  // Indicate that no movie was added
+            Close();
+        }
+
+    }
+}
---
+++ TrackFlix/FilterMovieWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TrackFlix1;
+
+namespace TrackFlix
+{
+
+    public partial class FilterMovieWindow : Window
+    {
+        public Movie MovieFilter { get; set; }
+        public FilterMovieWindow() // Reference to MainWIndow
+        {
+            InitializeComponent();
+        }
+
+        private void OnFilterMovieClick(object sender, RoutedEventArgs e)
+        {
+            // Code here and logic here
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            /********************************************
+           * Indicates that the user canceled the operation,
+           * preventing any movie from being updated, and closes the window.
+           ********************************************/
+
+            DialogResult = false;  // Indicate that no movie was added
+            Close();
+        }
+    }
+}
