49b17261e295320859c0bec79c6a06ee6aac3581
Aron Limos <157419134+AronLimos@users.noreply.github.com>
2024-11-25 20:34:32 -0700

    Added Design Logo and Filter Window

--- TrackFlix/obj/Debug/net8.0-windows/TrackFlix_Content.g.cs
+++ TrackFlix/obj/Debug/net8.0-windows/TrackFlix_Content.g.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/netflix_icon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/tracflix(t).ico")]
 
 
